66c82dccfb68a2aaf17e2518ca380f2ce3c5ccef
vitotankian <108244048+vitotankian@users.noreply.github.com>
2025-09-19 23:20:03 +0200

    test

--- js/tree.js
+++ js/tree.js
@@ -6,6 +6,15 @@ var layoutInfo = {
     treeLayout: ""
 }
 
+// This is the core definition of the layer tree.
+// It MUST be defined before tree-tab is created.
+// The game loop iterates over this array to update and display layers.
+var TREE_LAYERS = [
+    ["r"], // Row 0: The Rest layer
+    ["s"]  // Row 1: The Sleep layer
+];
+
+
 // A "ghost" layer which offsets other layers in the tree
 addNode("blank", {
     layerShown: "ghost",
@@ -18,10 +27,3 @@ addLayer("tree-tab", {
     previousTab: "",
     leftTab: true,
 })
-
-// This is the core definition of the layer tree.
-// The game loop iterates over this array to update and display layers.
-var TREE_LAYERS = [
-    ["r"], // Row 0: The Rest layer
-    ["s"]  // Row 1: The Sleep layer
-];
